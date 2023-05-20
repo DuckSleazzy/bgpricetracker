f8e844bccc517da1e669b7c3c5ad088b3b925690
DuckSleazzy <65159920+DuckSleazzy@users.noreply.github.com>
2023-05-21 00:19:58 +0530

    Add files via upload
    > fixed a small line of code, unsure why I added "ASUS" and "4070" in primeABGB crawler
    > Updated docstring
    > Windows are now properly sized, with consistent font sizes. They cannot be resized, and they popup in the center of the screen.
    > updated sleep function. Now the app checks instantly at first, then every 10 minutes afterwards.

--- bg_price_tracker.py
+++ bg_price_tracker.py
@@ -1,17 +1,10 @@
 '''
 SG
-May 13th 2023
+May 20th 2023
 python 3.10.10
-pyinstaller 5.1
-requests 2.28.1
-bs4 4.11.1
-tk 8.6.12
-playsound 1.2.2
-json ???
-re ???
+compiled using pyinstaller 5.1
 
 '''
-
 import re
 import requests
 import bs4
@@ -37,14 +30,19 @@ def popup_window(current_value):
     playsound("whatever.mp3")
     popup=Tk()
     popup.title("")
-    popup.geometry('400x200')
-    price_drop_label=Label(popup, text="Price has dropped for the desired product!",font=("Arial", 13))
+    screen_width = popup.winfo_screenwidth()
+    screen_height = popup.winfo_screenheight()
+    x = (screen_width - 400) // 2
+    y = (screen_height - 200) // 2
+    popup.geometry(f'400x200+{x}+{y}')
+    popup.resizable(False,False)
+    price_drop_label=Label(popup, text="Price has dropped for the desired product!", font=("Arial", 13))
     price_drop_label.pack(pady=2)
-    current_price_label=Label(popup, text=f"Current price: {current_value}",font=("Arial",13))
+    current_price_label=Label(popup, text=f"Current price: {current_value}", font=("Arial",13))
     current_price_label.pack(pady=2)
-    button=Button(popup, text="Okay",font=("Arial", 13),command=popup.destroy)
+    button=Button(popup, text="Okay", font=("Arial", 13), command=popup.destroy)
     button.pack(pady=2)
-    open_button = Button(popup, text="Open Link", font=("Arial", 13),command=open_link)
+    open_button = Button(popup, text="Open Link", font=("Arial", 13), command=open_link)
     open_button.pack(pady=2)
     popup.lift()
     popup.attributes('-topmost', True)
@@ -54,11 +52,16 @@ def popup_window(current_value):
 def error_window_func():
     error_window=Tk()
     error_window.title("Error!")
-    error_window.geometry("400x200")
-    error_label=Label(error_window,text="Please enter a valid link!")
-    error_label.pack()
-    try_again_button=Button(error_window,text="Try Again",command=asker.mainloop)
-    try_again_button.pack()
+    screen_width = error_window.winfo_screenwidth()
+    screen_height = error_window.winfo_screenheight()
+    x = (screen_width - 200) // 2
+    y = (screen_height - 100) // 2
+    error_window.geometry(f'200x100+{x}+{y}')
+    error_window.resizable(False,False)
+    error_label=Label(error_window,text="Please enter a valid link!",font=("Arial", 13))
+    error_label.pack(pady=12)
+    try_again_button=Button(error_window,text="Try Again", font=("Arial", 13), command=asker.mainloop)
+    try_again_button.pack(pady=2) 
     error_window.mainloop()
 
 # function to only enter digits in price module
@@ -66,36 +69,43 @@ def validate_price_entry(new_value):
     if new_value.isdigit() or new_value == "":
         return True
     else:
-        return False
+        return False       
 
 # creating asking window
 asker=Tk()
-asker.title("Input Example")
-asker.geometry("200x200")
+asker.title("")
+screen_width = asker.winfo_screenwidth()
+screen_height = asker.winfo_screenheight()
+x = (screen_width - 200) // 2
+y = (screen_height - 155) // 2
+asker.geometry(f"200x155+{x}+{y}")
+asker.resizable(False,False)
 
 link_value=StringVar()
 price_value=IntVar()
 
 # creating label and textbox for link and price
-link_label=Label(asker, text="Link: \n")
-link_label.pack()
-link_entry=Entry(asker, width=20, textvariable=link_value)
-link_entry.pack()
-price_label=Label(asker, text="Price: \n")
-price_label.pack()
-price_entry=Entry(asker, width=20, textvariable=price_value, validate="key", validatecommand=(asker.register(validate_price_entry), "%P"))
-price_entry.pack()
+link_label=Label(asker, text="Link:", font=("Arial", 13))
+link_label.pack(pady=2)
+link_entry=Entry(asker, width=20, textvariable=link_value, font=("Arial", 13))
+link_entry.pack(pady=2)
+price_label=Label(asker, text="Price:", font=("Arial", 13))
+price_label.pack(pady=2)
+price_entry=Entry(asker, width=20, textvariable=price_value, font=("Arial", 13), validate="key", validatecommand=(asker.register(validate_price_entry), "%P"))
+price_entry.pack(pady=2)
 
 # creating submit button
-submit_button=Button(asker, text="Submit", command=submit)
-submit_button.pack()
+submit_button=Button(asker, text="Submit", command=submit, font=("Arial",13))
+submit_button.pack(pady=2)
 
 asker.bind('<Return>', submit)
+asker.lift()
+asker.attributes('-topmost', True)
+asker.after_idle(asker.attributes, '-topmost', False)
 asker.mainloop()
 
 # Main loop
 while True:
-    time.sleep(3)
     try:
         result=requests.get(link_value.get())
         soup=bs4.BeautifulSoup(result.text,"lxml")
@@ -104,7 +114,7 @@ while True:
         if 'prime' in link_value.get():
             price_tag_list=soup.select("script",type="rocketlazyloadscript")
             for tag in price_tag_list:
-                if 'ASUS' and '4070' and '"price":' in tag.text:
+                if '"price":' in tag.text:
                     price_match=re.search(r'"price":(\d+)',tag.text)
                     current_value=int(price_match.group(1))
                     break
@@ -114,6 +124,7 @@ while True:
                 popup_window(current_value)
                 break
             else:
+                time.sleep(600)
                 continue
 
         # MDComputers logic
@@ -123,6 +134,7 @@ while True:
                 popup_window(current_value)
                 break
             else:
+                time.sleep(600)
                 continue
 
         # Vedant logic
@@ -134,6 +146,7 @@ while True:
                 popup_window(current_value)
                 break
             else:
+                time.sleep(600)
                 continue
     except:
         error_window_func()
